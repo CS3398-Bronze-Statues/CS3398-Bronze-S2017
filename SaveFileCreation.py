ff4396b0482086eb839b32c736d80bc116242900
krodbobcat <k_r84@txstate.edu>
2017-04-03 16:07:00 -0500

    Save File Creation

--- SaveFileCreation.py
+++ SaveFileCreation.py
@@ -5,6 +5,10 @@ and stores it into the corresponding key (string).
 import shelve
 
 shelfFile = shelve.open('saved_game_filename')
+shelfFile['character'] = save_character
+shelfFile['inventory'] = save_inventory
+shelfFile['zone'] = save_zone
+shelfFile['room'] = save_room
 """WILL ADD MORE WHEN STORY IS FINISHED"""
 
 shelfFile.close()
