f97123678cb4a7c58aee631e9b43aee6b553ee06
krodbobcat <k_r84@txstate.edu>
2017-04-26 15:38:18 -0500

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,56 +1,47 @@
 from Character import Character
-from SaveFileCreation import GetPlayerInfo
-from SaveFileCreation import Save
+from StringParser import StringParser
+from Maze import Maze
 import shelve
 
 class MainMenu(object):
     def __init__(self):
-        self.character = None
+        self.parser = None
 
     def menu(self):
-        while(True):
+        while True:
             print("Welcome to our Python Game!\n\n[1] New Game\n[2] Load Game\
             \n[3] How to Play\n[Anything else] Exit\n")
             choice = input("What'll it be? ")
-            if(choice == str(1)):
-                name = input("Enter new character's name: ")
-                gender = ""
-                while(True):
-                    gender = input("Is your character male or female? ")
-                    if(gender.lower() == "male" or gender.lower() == "female"):
-                        gender = gender.lower()
-                        break
-                    else:
-                        print("Please type either \"male\" or \"female\"\n")
-
-                self.character = Character(name, gender)
-                print(self.character.toString())
-                print("This option starts a new game!\n")
-                
-                """
-                Saving here makes sure that a new save file is created
-                for the new game. Basically starts on a blank slate and
-                erases previous data.
-                """
-                GetPlayerInfo(self.character)
-                Save()
-                print("New Save File Created!\n")
-            elif(choice == str(2)):
+            if choice == str(1):
+                self.parser = StringParser(Maze(Character()))
+                break
+            elif choice == str(2):
                 print("This option loads a saved game!\n")
                 saveGameShelfFile = shelve.open('save_game_data')
                 if 'character' in saveGameShelfFile:
-                    self.character = saveGameShelfFile['character']
+                    load_character = saveGameShelfFile['character']
+                    self.parser = StringParser(Maze(load_character))
                     print('Game loaded.\n')
-                    
-                    """Prints character data to ensure save file has correct data"""
-                    print(self.character.toString())
+                    break
                 else:
                     print('\n\nThere is no previously saved game.\n')
-            elif(choice == str(3)):
-                print("This option explains the game!\n")
+            elif choice == str(3):
+                print("This is a text based adventure game!  You are trapped in a labyrinth and must escape to save\
+                 your people.  Type commands to play the game.\n")
             else:
-                print("This option exits!\n")
                 break
 
+    def play(self):
+        self.menu()
+        if self.parser is None:
+            print("Goodbye!")
+        else:
+            while True:
+                self.parser.maze.update()
+                print(self.parser.maze.output)
+                while self.parser.parse(input("What do you do?")) != True:
+                    print("Try again...")
+
+
 thing = MainMenu()
-thing.menu()
+thing.play()
