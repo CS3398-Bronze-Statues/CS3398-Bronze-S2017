9cf09c087da52f31b8d7ec6f65d9207fdc523381
cc1778 <cc1778@txstate.edu>
2017-04-26 14:03:39 -0500

    Update StringParser.py

--- StringParser.py
+++ StringParser.py
@@ -3,8 +3,8 @@ function.  It is a starting template for strings to parse.  Just need to add fun
 based on the corresponding command detected."""
 
 class StringParser(object):
-    def __init__(self):
-        pass
+    def __init__(self, maze):
+        self.maze = maze
 
     def parse(self, string):
         self.strings = string.split(" ")
@@ -12,44 +12,47 @@ class StringParser(object):
         for i in range(0,len(self.strings)):
             if self.strings[i].lower() == "save":
                 print("Saved the game.")
-                return
+                return True
             elif self.strings[i].lower() == "quit":
                 print("Quit the game.")
-                return
+                return True
             elif self.strings[i].lower() == "move" or self.strings[i].lower() == "go"\
                 or self.strings[i].lower() == "walk" or self.strings[i].lower() == "run":
                 for j in range(0,len(self.strings)):
                     if self.strings[j].lower() == "north" or self.strings[j].lower() == "up":
-                        print("Going north/up")
-                        return
+                        if self.maze.north == True:
+                            self.maze.player.y += 1
+                            self.maze.update()
+                            return True
+                        else:
+                            print("You can't go that way...")
+                            return False
                     elif self.strings[j].lower() == "east" or self.strings[j].lower() == "right":
-                        print("Going east/right")
-                        return
+                        if self.maze.east == True:
+                            self.maze.player.x += 1
+                            self.maze.update()
+                            return True
+                        else:
+                            print("You can't go that way...")
+                            return False
                     elif self.strings[j].lower() == "west" or self.strings[j].lower() == "left":
-                        print("Going west/left")
-                        return
+                        if self.maze.west == True:
+                            self.maze.player.x -= 1
+                            self.maze.update()
+                            return True
+                        else:
+                            print("You can't go that way...")
+                            return False
                     elif self.strings[j].lower() == "south" or self.strings[j].lower() == "down":
-                        print("Going south/down")
-                        return
+                        if self.maze.south == True:
+                            self.maze.player.y -= 1
+                            self.maze.update()
+                            return True
+                        else:
+                            print("You can't go that way...")
+                            return False
             elif self.strings[i].lower() == "inspect" or self.strings[i].lower() == "view"\
-                or self.strings[i].lower() == "open" or (self.strings[i].lower() == "look"\
-                                                         and self.strings[i+1].lower() == "at"):
+                or self.strings[i].lower() == "open" or \
+                    (self.strings[i].lower() == "look" and self.strings[i+1].lower() == "at"):
                         #if item in zone or character list:
                             print("Inspecting an item")
-
-
-parser = StringParser()
-parser.parse("save")
-parser.parse("quit")
-parser.parse("go south")
-parser.parse("down go")
-parser.parse("move north")
-parser.parse("up move")
-parser.parse("walk west")
-parser.parse("left walk")
-parser.parse("east run")
-parser.parse("right run")
-parser.parse("inspect")
-parser.parse("view")
-parser.parse("open")
-parser.parse("look at")
