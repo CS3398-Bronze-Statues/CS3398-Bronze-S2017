6cb35567199361bea08d566eec15ca28c3d38359
cc1778 <cc1778@txstate.edu>
2017-04-26 18:21:23 -0500

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,5 +1,3 @@
-echo "testing MainMenu.py with MainMenuTest.txt"
-python MainMenu.py < MainMenuTest.txt
-echo "testing MainMenu.py build"
-python Character.py
 echo "testing Character.py build"
+python code/Character.py
+echo "done testing"
