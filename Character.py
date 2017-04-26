103ad866113be0f01df94a742279092613ee103d
krodbobcat <k_r84@txstate.edu>
2017-04-26 16:24:46 -0500

    Update for demo

--- Character.py
+++
@@ -1,5 +0,0 @@
-class Character(object):
-    def __init__(self):
-        self.weapon = False
-        self.x = 0
-        self.y = 0
--- tests.sh
+++
@@ -1,3 +0,0 @@
-echo "testing MainMenu.py with MainMenuTest.txt"
-python MainMenu.py < MainMenuTest.txt
-echo "testing Character.py build"
