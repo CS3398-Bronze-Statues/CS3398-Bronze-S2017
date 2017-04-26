aee80aef57a7f0bb9fb64a75c71584df3a716a1c
cc1778 <cc1778@txstate.edu>
2017-04-26 18:29:00 -0500

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,3 +1,18 @@
+echo "beginning testing"
 echo "testing Character.py build"
-python3 code/Character.py
+python code/Character.py
+echo "testing Item.py build"
+python code/Item.py
+echo "testing Maze.py build"
+python code/Maze.py
+echo "testing Monsters.py build"
+python code/Monsters.py
+echo "testing SaveFileCreation.py build"
+python code/SaveFileCreation.py
+echo "testing StringParser.py build"
+python code/StringParser.py
+echo "testing Zones.py build"
+python code/Zones.py
+#echo "testing combatSystem.py build"
+#python code/combatSystem.py
 echo "done testing"
