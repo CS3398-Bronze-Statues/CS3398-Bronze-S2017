bf1a3c8649661d817583762d4047474fd59dcc8f
mcs152 <mcs152@txstate.edu>
2017-04-26 17:12:05 -0500

    Create tests.sh

---
+++ tests.sh
@@ -0,0 +1,7 @@
+echo "testing MainMenu.py with MainMenuTest.txt"
+python MainMenu.py < MainMenuTest.txt
+echo "testing Character.py build"
+python Character.py
+echo "testing StringParser.py with built in test commands"
+python StringParser.py
+echo "done testing"
