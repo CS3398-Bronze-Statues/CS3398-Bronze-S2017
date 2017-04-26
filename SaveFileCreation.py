176d73aa3df015da927bcdea94e865ac08ec71df
krodbobcat <k_r84@txstate.edu>
2017-04-25 22:26:51 -0500

    Fixed incorrect saving
    Wasn't saving things correctly.

--- SaveFileCreation.py
+++ SaveFileCreation.py
@@ -5,15 +5,15 @@ and stores it into the corresponding key (string).
 import shelve
 from Character import Character
 
-player_char = Character("", "")
-
 """
-Gets player info before saving
+Gets player info before saving. Used for instances where Save is in a file/module
+without any player info.
 """
 def GetPlayerInfo(get_char):
+    global player_char
     player_char = get_char
 
 def Save():
-    shelfFile = shelve.open('saved_game_data')
+    shelfFile = shelve.open('save_game_data')
     shelfFile['character'] = player_char
     shelfFile.close()
