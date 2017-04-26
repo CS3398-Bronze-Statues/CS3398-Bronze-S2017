f068d4b4b1517362429df450ce35d5e090c83a01
mcs152 <mcs152@txstate.edu>
2017-04-26 18:19:29 -0500

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,7 +1,5 @@
 echo "testing MainMenu.py with MainMenuTest.txt"
 python MainMenu.py < MainMenuTest.txt
-echo "testing Character.py build"
+echo "testing MainMenu.py build"
 python Character.py
-echo "testing StringParser.py with built in test commands"
-python StringParser.py
-echo "done testing"
+echo "testing Character.py build"
