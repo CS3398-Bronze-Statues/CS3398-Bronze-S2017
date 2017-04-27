9384ad2cb032785fd291478d9d1ec991a36b115c
cc1778 <cc1778@txstate.edu>
2017-04-26 21:16:02 -0500

    Update and rename tests.sh to testBuilds.sh

--- testBuilds.sh
+++ testBuilds.sh
@@ -3,6 +3,8 @@ echo "testing Character.py build"
 python code/Character.py
 echo "testing Item.py build"
 python code/Item.py
+echo "testing MainMenu.py build"
+python code/MainMenu.py
 echo "testing Maze.py build"
 python code/Maze.py
 echo "testing Monsters.py build"
@@ -11,8 +13,6 @@ echo "testing SaveFileCreation.py build"
 python code/SaveFileCreation.py
 echo "testing StringParser.py build"
 python code/StringParser.py
-echo "testing Zones.py build"
-python code/Zones.py
 echo "testing combatSystem.py build"
 python code/combatSystem.py
 echo "done testing"
